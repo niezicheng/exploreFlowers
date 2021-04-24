ff388bc63a00861c8532ca733152e61573aa0cad
niezicheng <2429705898@qq.com>
2021-04-24 18:54:07 +0800

    feat: hide warn、show network and rresponse data

--- src/utils/request.js
+++ src/utils/request.js
@@ -20,7 +20,7 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(function (response) {
   Toast.hideLoading();
   // 对响应数据做点什么
-  return response;
+  return response.data;
 }, function (error) {
   // 对响应错误做点什么
   return Promise.reject(error);
