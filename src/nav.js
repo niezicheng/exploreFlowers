082f8d948f60705c50a2a09985507ac7ee2a7823
niezicheng <2429705898@qq.com>
2021-05-19 09:31:05 +0800

    feat: 极光配置 Android 添加

--- src/nav.js
+++ src/nav.js
@@ -3,15 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pages/account/login';
 import UserInfo from './pages/account/userInfo';
+import Demo from './pages/account/Demo';
 
 const Stack = createStackNavigator();
 
 function Nav() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="UserInfo">
+      <Stack.Navigator headerMode="none" initialRouteName="Demo">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="UserInfo" component={UserInfo} />
+        <Stack.Screen name="Demo" component={Demo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/pages/account/Demo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text } from "react-native";
+import JMessage from "jmessage-react-plugin";
+
+class Demo extends React.Component {
+  componentDidMount() {
+    JMessage.init({
+      'appkey': 'c0c08d3d8babc318fe25bb0c',
+      'isOpenMessageRoaming': true,
+      'isProduction': false,
+      'channel': ''
+    })
+
+    JMessage.login({
+      username: "18665711956",
+      password: "18665711956"
+    }, (res) => {
+      console.log("登录成功");
+      console.log(res);
+    }, (err) => {
+      console.log("登录失败");
+      console.log(err);
+    })
+
+  }
+  render() {
+    return (
+      <View>
+        <Text>goods</Text>
+      </View>
+    );
+  }
+}
+export default Demo;
